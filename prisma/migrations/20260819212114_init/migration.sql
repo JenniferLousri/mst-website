30073b34a53729cf7eaf40348ccd0ab1610342ce
JenniferLousri1 <jennifer.lousri@gmail.com>
2026-08-20 04:23:14 +0700

    Update Prisma migration for PostgreSQL

--- prisma/migrations/20260819212114_init/migration.sql
+++ prisma/migrations/20260819212114_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "Inquiry" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "fullName" TEXT NOT NULL,
     "company" TEXT NOT NULL,
     "email" TEXT NOT NULL,
@@ -8,5 +8,7 @@ CREATE TABLE "Inquiry" (
     "service" TEXT NOT NULL,
     "message" TEXT NOT NULL,
     "status" TEXT NOT NULL DEFAULT 'Baru',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Inquiry_pkey" PRIMARY KEY ("id")
 );
